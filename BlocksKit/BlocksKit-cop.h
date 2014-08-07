801f4af2a2c6f9505977081200c65f85f4f5fab8
Serhey Tkachenko <serhey.tkachenko@gmail.com>
2014-08-07 17:48:57 +0300

    Create BlocksKit-cop.h
    Added header file for BlocksKit-cop podspec

---
+++ BlocksKit/BlocksKit-cop.h
@@ -0,0 +1,43 @@
+//
+//  BlocksKit
+//
+//  The Objective-C block utilities you always wish you had.
+//
+//  Copyright (c) 2011-2012, 2013-2014 Zachary Waldowski
+//  Copyright (c) 2012-2013 Pandamonia LLC
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import <BlocksKit-cop/NSArray+BlocksKit.h>
+#import <BlocksKit-cop/NSDictionary+BlocksKit.h>
+#import <BlocksKit-cop/NSIndexSet+BlocksKit.h>
+#import <BlocksKit-cop/NSInvocation+BlocksKit.h>
+#import <BlocksKit-cop/NSMutableArray+BlocksKit.h>
+#import <BlocksKit-cop/NSMutableDictionary+BlocksKit.h>
+#import <BlocksKit-cop/NSMutableIndexSet+BlocksKit.h>
+#import <BlocksKit-cop/NSMutableOrderedSet+BlocksKit.h>
+#import <BlocksKit-cop/NSMutableSet+BlocksKit.h>
+#import <BlocksKit-cop/NSObject+BKAssociatedObjects.h>
+#import <BlocksKit-cop/NSObject+BKBlockExecution.h>
+#import <BlocksKit-cop/NSObject+BKBlockObservation.h>
+#import <BlocksKit-cop/NSOrderedSet+BlocksKit.h>
+#import <BlocksKit-cop/NSSet+BlocksKit.h>
+#import <BlocksKit-cop/NSTimer+BlocksKit.h>
+#import <BlocksKit-cop/BKMacros.h>
